4dc9ee4d9b5dc445df487f571de89498e1165103
Bernadette Vogl <bernadette.vogl@edumail.at>
2021-04-14 12:11:24 +0200

    21: add basic styles [VB,ST]

--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/MainActivity.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.tugraz.asd.modernnewsgroupapp
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.TypedValue
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
+    private var _customFontTextView: TextView? = null
+    private var _typeFace:           Typeface? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        //textView = findViewById(R.id.toolbar)
+        //textView.typeface = ResourcesCompat.getFont(this, )
+        //textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50.toFloat())
+
+        this._initializeResources()
+        this._initializeGUI()
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    private fun _initializeResources() {
+        this._customFontTextView = this.findViewById(R.id.headline)
+        this._typeFace           = ResourcesCompat.getFont(this.applicationContext, R.font.caveat_bold)
+    }
+
+    private fun _initializeGUI() {
+        this._customFontTextView!!.setTypeface(this._typeFace, Typeface.NORMAL)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
