39eb72900d7a3c778ab6815bf946b00e4bb3c5c7
Tamara Steinwender <tamara.steinwender@tugraz.at>
2021-04-19 20:21:14 +0200

    MNA-Task-001 add an espresso ui test [TA,SP]

---
+++ app/src/test/java/com/tugraz/asd/modernnewsgroupapp/AddNewsgroupTest.kt
@@ -0,0 +1,13 @@
+package com.tugraz.asd.modernnewsgroupapp
+
+import org.junit.Assert
+import org.junit.Test
+
+class AddNewsgroupTest {
+    @Test
+    fun click_through() {
+        //onView(withId(R.id.editTextUserInput))
+         //   .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard())
+       // onView(withId(R.id.changeTextButton)).perform(click())
+    }
+}
