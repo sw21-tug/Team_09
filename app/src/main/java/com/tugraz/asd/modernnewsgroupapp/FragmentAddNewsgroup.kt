27c1e2f0ee62db7595ec87def9695267bb723e5c
Tamara Steinwender <tamara.steinwender@tugraz.at>
2021-04-21 09:47:39 +0200

    27: Add basic functional test for add news groups and a button click handler [ST,TA]

--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentAddNewsgroup.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentAddNewsgroup.kt
@@ -7,25 +7,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.tugraz.asd.modernnewsgroupapp.databinding.ActivityAddNewsgroupBinding
+import com.tugraz.asd.modernnewsgroupapp.databinding.FragmentAddNewsgroupBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FragmentAddNewsgroup : Fragment() {
 
+    private lateinit var binding: FragmentAddNewsgroupBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_newsgroup, container, false)
+        binding = FragmentAddNewsgroupBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.buttonSubscribe.setOnClickListener() {
+            onButtonSubscribeClick()
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        view.findViewById<Button>(R.id.button_subscribe).setOnClickListener {
-            findNavController().navigate(R.id.action_AddNewsgroup_to_Subscribe)
-        }
+    fun onButtonSubscribeClick() {
+        val email = binding.editTextEmail.text
+        val name = binding.editTextName
+        val server = binding.editTextNewsgroupServer
+        val serverAlias = binding.editTextServerAlias
+       // findNavController().navigate(R.id.action_AddNewsgroup_to_Subscribe)
+
     }
 }
