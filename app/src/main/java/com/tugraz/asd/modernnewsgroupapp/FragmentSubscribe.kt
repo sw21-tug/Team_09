d5fae22ece8e1cf97ba8705795c5b23967e38269
Patrick Struger <pstruger@student.tugraz.at>
2021-05-04 18:42:13 +0200

    MNA-003 Created custom list hierarchy, adapted newsgroup filter and data passing [SP, JI]

--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentSubscribe.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentSubscribe.kt
@@ -1,23 +1,22 @@
 package com.tugraz.asd.modernnewsgroupapp
 
-import CustomExpandableListAdapter
+import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.ExpandableListAdapter
 import android.widget.LinearLayout
-import androidx.core.view.forEach
+import android.widget.TextView
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.tugraz.asd.modernnewsgroupapp.databinding.FragmentSubscribeBinding
-import java.util.*
+import com.tugraz.asd.modernnewsgroupapp.vo.Newsgroup
 
 
 /**
@@ -29,15 +28,13 @@ class FragmentSubscribe : Fragment() {
     private lateinit var binding: FragmentSubscribeBinding
     private lateinit var viewModel: ServerObseravble
 
-    private var adapter: ExpandableListAdapter? = null
-    private var titleList: List<String>? = null
-    private var expandableListItem = HashMap<String, List<String>>()
+    // use this list for subscribed newsgroups
+    private var newsgroupList: ArrayList<Newsgroup>? = null
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-
 
         binding = FragmentSubscribeBinding.inflate(layoutInflater)
 
@@ -53,131 +50,117 @@ class FragmentSubscribe : Fragment() {
             onButtonBackClick()
         }
 
-        binding.editTextGroupFilter.addTextChangedListener(filterTextWatcher);
+        binding.editTextGroupFilter.addTextChangedListener(filterTextWatcher)
 
         viewModel.data.observe(viewLifecycleOwner, Observer {
-            for (newsgroup in viewModel.data.value?.newsGroups!!) {
+            val newsgroupsToAdd = ArrayList<Newsgroup>()
+            newsgroupList = viewModel.data.value?.newsGroups!! as ArrayList<Newsgroup>
+            for (newsgroup in newsgroupList!!) {
 
                 if (newsgroup.isSubgroup()) {
                     newsgroup.setParentNewsgroup()
-                    if (viewModel.data.value?.newsGroups!!.any { it.name.equals(newsgroup.parent) }) {
-                        addToExpandableList(newsgroup.parent!!, newsgroup.parent!!)
+                    newsgroup.setHierarchyLevel()
+
+                    // add parent newsgroup which does not exist (no subscribe possibility)
+                    if (!newsgroupsToAdd.any{it.name == newsgroup.parent} && !newsgroupList!!.any {it.name == newsgroup.parent}) {
+                        val new = Newsgroup(newsgroup.parent!!)
+                        new.setParentNewsgroup()
+                        new.setHierarchyLevel()
+                        newsgroupsToAdd.add(new)
                     }
-                    addToExpandableList(newsgroup.parent!!, newsgroup.name)
                 } else {
-                    // TODO: if it is not a subgroup
-                    //addToExpandableList("No subgroups", newsgroup.name)
-                    expandableListItem[newsgroup.name] = ArrayList()
+                    // newsgroup without subgroups
+                    newsgroup.setHierarchyLevel()
                 }
             }
-
-            val expandableListView = binding.expandableListView
-            titleList = ArrayList(expandableListItem.keys)
-            this.adapter = CustomExpandableListAdapter(requireContext(), titleList as ArrayList<String>, expandableListItem)
-            expandableListView.setAdapter(this.adapter)
+            newsgroupList!!.addAll(newsgroupsToAdd)
+            createHierarchyView(binding.viewSubscribe, newsgroupList!!, 0)
         })
 
         return binding.root
     }
 
-    private fun addToExpandableList(key: String, value: String) {
-        if (expandableListItem.containsKey(key)) {
-            val list = expandableListItem[key] as MutableList<String>
-            list.add(value)
-            expandableListItem[key] = list
-        } else {
-            val children: MutableList<String> = ArrayList()
-            children.add(value)
-            expandableListItem[key] = children
+    private fun createHierarchyView(parentLayout: LinearLayout, newsgroups: List<Newsgroup>, level: Int) {
+
+        for (item in newsgroups.filter { it.hierarchyLevel == level }) {
+
+            var layout = layoutInflater.inflate(R.layout.row_first, null)
+            val listTitleTextView = layout!!.findViewById<TextView>(R.id.textViewName)
+            listTitleTextView.setTypeface(null, Typeface.BOLD)
+            listTitleTextView.text = item.name
+
+            // recursive level down if newsgroup has subgroups
+            if (newsgroupList!!.any { it.parent == item.name }) {
+                val nextLevelLayout: LinearLayout = layout.findViewById(R.id.linear_scroll)
+                //nextLevelLayout.visibility = View.GONE
+
+                // set onClick listener for expanding / reducing elements
+                listTitleTextView.setOnClickListener {
+                    if (nextLevelLayout.visibility == View.GONE)
+                        nextLevelLayout.visibility = View.VISIBLE
+                    else
+                        nextLevelLayout.visibility = View.GONE
+                }
+                createHierarchyView(nextLevelLayout, newsgroupList!!.filter { it.parent == item.name }, level+1)
+            } else {
+                // use checkbox layout instead to subscribe / unsubscribe
+                layout = layoutInflater.inflate(R.layout.checkbox_item, null)
+                val checkbox = layout!!.findViewById<CheckBox>(R.id.checkBox)
+                checkbox.text = item.name
+
+                // subscribe / unsubscribe newsgroup if checked / unchecked
+                checkbox.setOnCheckedChangeListener { buttonView, isChecked ->
+                    if (isChecked) {
+                        newsgroupList!!.find { it.name == checkbox.text }?.subscribe()
+                    } else {
+                        newsgroupList!!.find { it.name == checkbox.text }?.unsubscribe()
+                    }
+                }
+            }
+            parentLayout.addView(layout)
         }
     }
 
-    // TODO: filter through expandable list
     private val filterTextWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
         }
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
         }
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            /*val checkboxes = binding.expandableListView.children
+            recursiveNewsgroupFilter(binding.viewSubscribe, s)
+        }
+    }
 
-            for (box in checkboxes) {
-                val c: CheckBox = box as CheckBox;
-
-                if(!c.text.contains(s!!)) {
-                    c.visibility = View.GONE;
+    fun recursiveNewsgroupFilter(parent: LinearLayout, s: CharSequence?) {
+        for (child in parent.children) {
+            if (child is LinearLayout) {
+                recursiveNewsgroupFilter(child, s)
+                // get rid of newsgroups not matching filter text
+                if (!parent.children.any{it is LinearLayout && it.visibility == View.VISIBLE}) {
+                    parent.visibility = View.GONE
                 } else {
-                    c.visibility = View.VISIBLE;
+                    parent.visibility = View.VISIBLE
                 }
-            }*/
-
-            //for (i in 0 until adapter!!.groupCount) binding.expandableListView.expandGroup(i)
-
-            for (i in 0 until (binding.expandableListView as ViewGroup).childCount) {
-                val nextChild = (binding.expandableListView as ViewGroup).getChildAt(i)
-                if (nextChild is CheckBox) {
-                    if (!nextChild.text.contains(s!!)) {
-                        nextChild.visibility = View.GONE
+            } else {
+                if (child is CheckBox) {
+                    if(!child.text.contains(s!!)) {
+                        parent.visibility = View.GONE
                     } else {
-                        nextChild.visibility = View.VISIBLE
+                        parent.visibility = View.VISIBLE
                     }
                 }
             }
         }
     }
 
-    // TODO: get all checkboxes from expandable list
     fun onButtonFinishClick() {
-        binding.viewSubscribe.forEach {
-            val checkbox = it as CheckBox
-            if(checkbox.isChecked) {
-                val ng = viewModel.data.value?.newsGroups?.filter{ng -> checkbox.text == ng.name}
-                if (ng != null) {
-                    ng.elementAt(0).subscribed = true
-                }
-            }
-        }
+        /*
+         TODO use class attribute "newsgroupList" for further processing
+         all of subscribed newsgroups are saved in there
+         */
     }
 
     private fun onButtonBackClick() {
         findNavController().navigate(R.id.action_FragmentSubscribe_to_FragmentAddNewsgroup)
-    }
-
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_subscribe).setOnClickListener {
-            findNavController().navigate(R.id.action_AddNewsgroup_to_Subscribe)
-        }
-    }*/
-
-    private fun createListEntry(newsgroup: String, padding: Int) {
-        val check = CheckBox(activity)
-        check.text = newsgroup
-        check.setPadding(padding, 10, 10, 10)
-        val params = LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        params.setMargins(padding, 5, 5, 5)
-        params.gravity = Gravity.NO_GRAVITY
-        check.layoutParams = params
-        check.gravity = Gravity.CENTER
-
-        //binding.viewSubscribe.addView(check)
-    }
-
-    private fun createDropDownEntries(title: String, subgroups: MutableList<String>) {
-        /*
-        Create a Spinner/Dropdown of all subgroups.
-        E.G.
-        Expand lv and then we'll be able to see
-        lv.analysis
-        lv.algorithmen etc.
-         */
-
-//        val listView = ListView(activity)
-
-
     }
 }
--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/vo/Newsgroup.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/vo/Newsgroup.kt
@@ -1,21 +1,34 @@
 package com.tugraz.asd.modernnewsgroupapp.vo
 
+import kotlin.text.count as count
+
 data class Newsgroup (var name: String){
     var parent: String? = null
+    var hierarchyLevel: Int? = null
     var subscribed: Boolean = false
 
     fun hasSubgroup(): Boolean {
-        return this.parent != null
+        return parent != null
     }
 
-    // if newsgroup has at least two dots in its name -> indicates subgroup
+    fun subscribe() {
+        subscribed = true
+    }
+
+    fun unsubscribe() {
+        subscribed = false
+    }
+
+    // if newsgroup has at least a dot in its name -> indicates subgroup
     fun isSubgroup(): Boolean {
-        //println("Subgroup: $name")
-        return name.count{ character -> character == '.' } >= 2
+        return name.filter { it == '.' }.count() >= 1
     }
 
     fun setParentNewsgroup() {
-        var splitted: List<String> = name.split('.')
-        parent =  splitted[0] + "." + splitted[1]
+        parent = name.substringBeforeLast(".")
+    }
+
+    fun setHierarchyLevel() {
+        hierarchyLevel = name.filter { it == '.' }.count()
     }
 }
