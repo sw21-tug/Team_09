7dd23d63a10a1097f5330f73bf848543baa12c74
Patrick Struger <pstruger@student.tugraz.at>
2021-05-05 12:05:52 +0200

    MNA-008 Adapted UI tests [SP, VB]

--- app/src/androidTest/java/com/tugraz/asd/modernnewsgroupapp/EditNewsgroupAliasTest.kt
+++ app/src/androidTest/java/com/tugraz/asd/modernnewsgroupapp/EditNewsgroupAliasTest.kt
@@ -1,15 +1,17 @@
 package com.tugraz.asd.modernnewsgroupapp
 
-import androidx.lifecycle.ViewModelProviders
+import android.view.View
+import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.tugraz.asd.modernnewsgroupapp.vo.NewsgroupServer
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers.containsString
 import org.hamcrest.Matchers.not
 import org.junit.Rule
@@ -27,14 +29,14 @@ class EditNewsgroupAliasTest {
     var  rule: ActivityScenarioRule<ActivityAddNewsgroup> = ActivityScenarioRule<ActivityAddNewsgroup>(ActivityAddNewsgroup::class.java)
 
     private fun init() {
-        val inputName = onView(withId(R.id.editText_name)).check(matches(ViewMatchers.isDisplayed()))
-        val inputEmail = onView(withId(R.id.editText_email)).check(matches(ViewMatchers.isDisplayed()))
+        val inputName = onView(withId(R.id.editText_name)).check(matches(isDisplayed()))
+        val inputEmail = onView(withId(R.id.editText_email)).check(matches(isDisplayed()))
 
-        val inputServer = onView(withId(R.id.editText_newsgroupServer)).check(matches(ViewMatchers.isDisplayed()))
-        val inputAlias = onView(withId(R.id.editText_serverAlias)).check(matches(ViewMatchers.isDisplayed()))
+        val inputServer = onView(withId(R.id.editText_newsgroupServer)).check(matches(isDisplayed()))
+        val inputAlias = onView(withId(R.id.editText_serverAlias)).check(matches(isDisplayed()))
 
-        serverName = inputServer.toString()
-        serverAlias = inputAlias.toString()
+        serverName = getText(inputServer)
+        serverAlias = getText(inputAlias)
 
         inputName.perform(ViewActions.replaceText("test"), ViewActions.closeSoftKeyboard())
         inputEmail.perform(ViewActions.replaceText("test@test.at"), ViewActions.closeSoftKeyboard())
@@ -58,5 +60,24 @@ class EditNewsgroupAliasTest {
         val matchString = "news.KFGraz.at"
         onView(withId(R.id.button_edit_newsgroup)).perform(ViewActions.click())
         onView(withId(R.id.header_newsgroup_name)).check(matches(not(withText(containsString(matchString)))))
+    }
+
+    private fun getText(matcher: ViewInteraction): String {
+        var text = String()
+        matcher.perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "Text of the view"
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val tv = view as TextView
+                text = tv.text.toString()
+            }
+        })
+        return text
     }
 }
