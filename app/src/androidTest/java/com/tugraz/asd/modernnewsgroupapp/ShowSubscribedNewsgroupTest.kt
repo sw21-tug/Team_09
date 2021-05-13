84ef5fa658a5bd9ac41b8dbfe0b0a11007981cf5
Patrick Struger <struger93@gmx.at>
2021-05-13 22:38:10 +0200

    MNA-004 Fixed UI test cases, code and gradle cleanup [SP, JI]

--- app/src/androidTest/java/com/tugraz/asd/modernnewsgroupapp/ShowSubscribedNewsgroupTest.kt
+++ app/src/androidTest/java/com/tugraz/asd/modernnewsgroupapp/ShowSubscribedNewsgroupTest.kt
@@ -1,17 +1,11 @@
 package com.tugraz.asd.modernnewsgroupapp
 
-import android.view.View
-import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,27 +13,16 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ShowSubscribedNewsgroupTest {
 
-    private var serverName: String? = null
-    private var serverAlias: String? = null
-
     @Rule
     @JvmField
-    var  rule: ActivityScenarioRule<ActivityAddNewsgroup> = ActivityScenarioRule<ActivityAddNewsgroup>(ActivityAddNewsgroup::class.java)
+    var  rule: ActivityScenarioRule<ActivityAddNewsgroup> = ActivityScenarioRule(ActivityAddNewsgroup::class.java)
 
     private fun init() {
         val inputName = onView(withId(R.id.editText_name)).check(matches(isDisplayed()))
         val inputEmail = onView(withId(R.id.editText_email)).check(matches(isDisplayed()))
 
-        val inputServer = onView(withId(R.id.editText_newsgroupServer)).check(matches(isDisplayed()))
-        val inputAlias = onView(withId(R.id.editText_serverAlias)).check(matches(isDisplayed()))
-
         inputName.perform(replaceText("test"), closeSoftKeyboard())
         inputEmail.perform(replaceText("test@test.at"), closeSoftKeyboard())
-
-        inputAlias.perform(replaceText("AliasTest"), closeSoftKeyboard())
-
-        serverName = getText(inputServer)
-        serverAlias = getText(inputAlias)
 
         onView(withText("NEXT")).perform(click())
         onView(withText("tu-graz.algorithmen")).perform(click())
@@ -50,24 +33,5 @@ class ShowSubscribedNewsgroupTest {
     fun showSubscribedNewsgroupTest() {
         init()
         onView(withText("tu-graz.algorithmen")).check(matches(isDisplayed()))
-    }
-
-    private fun getText(matcher: ViewInteraction): String {
-        var text = String()
-        matcher.perform(object : ViewAction {
-            override fun getConstraints(): Matcher<View> {
-                return isAssignableFrom(TextView::class.java)
-            }
-
-            override fun getDescription(): String {
-                return "Text of the view"
-            }
-
-            override fun perform(uiController: UiController, view: View) {
-                val tv = view as TextView
-                text = tv.text.toString()
-            }
-        })
-        return text
     }
 }
--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentShowSubgroups.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentShowSubgroups.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -30,7 +29,7 @@ class FragmentShowSubgroups : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         viewModel = activity?.run {
             ViewModelProvider(this).get(ServerObservable::class.java)
@@ -38,7 +37,7 @@ class FragmentShowSubgroups : Fragment() {
 
         binding = FragmentShowSubgroupsBinding.inflate(layoutInflater)
 
-        viewModel.data.observe(viewLifecycleOwner, Observer {
+        viewModel.data.observe(viewLifecycleOwner, {
             controller = viewModel.data.value!!
         })
         controller = viewModel.data.value!!
