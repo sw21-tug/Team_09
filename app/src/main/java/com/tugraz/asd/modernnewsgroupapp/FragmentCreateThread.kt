a7e8985086589e08a01f83b3e5fb718a9b0e5ecd
Irfan Jahic <irfan.jahic@student.tugraz.at>
2021-05-26 11:58:11 +0200

    MNA-006 Trying to implement frontend of create thread and post message [JI,TA]

--- app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentCreateThread.kt
+++ app/src/main/java/com/tugraz/asd/modernnewsgroupapp/FragmentCreateThread.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import com.tugraz.asd.modernnewsgroupapp.databinding.FragmentCreateThreadBinding
+import com.tugraz.asd.modernnewsgroupapp.helper.Feedback
 import org.apache.commons.net.nntp.Article
 
 /**
@@ -19,25 +24,52 @@ class FragmentCreateThread : Fragment() {
     private lateinit var articles: ArrayList<Article>
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentCreateThreadBinding.inflate(layoutInflater)
 
+        binding.buttonSend.setOnClickListener {
+            onButtonSendClick()
+        }
+        viewModel = activity?.run {
+            ViewModelProvider(this).get(ServerObservable::class.java)
+        } ?: throw Exception("Invalid Activity")
+
+        viewModel.controller.observe(viewLifecycleOwner) {
+            controller = viewModel.controller.value!!
+            onControllerChange()
+        }
+        binding.buttonCloseThread.setOnClickListener {
+            findNavController().navigate(R.id.action_FragmentCreateThread_to_FragmentMessageThreads)
+        }
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel = activity?.run {
-            ViewModelProviders.of(this).get(ServerObservable::class.java)
-        } ?: throw Exception("Invalid Activity")
-        controller = viewModel.controller.value!!
-
+    fun onControllerChange() {
         if (controller.currentNewsgroup!!.alias.isNullOrEmpty()) {
             binding.headerCreateThread.setText(controller.currentNewsgroup!!.name)
         } else {
             binding.headerCreateThread.setText(controller.currentNewsgroup!!.alias)
+        }
+    }
+
+    fun onButtonSendClick() {
+        val subject = binding.threadSubject.text
+        val text = binding.threadMessage.text
+
+        if (subject.toString() == "" ) {
+            Feedback.showError(this.requireView(), getString(R.string.feedback_missing_subject))
+            return
+        } else if (text.toString() == "") {
+            Feedback.showError(this.requireView(), getString(R.string.feedback_missing_message))
+            return
+        }
+
+        if(!controller.postArticle(subject.toString(), text.toString())) {
+            Feedback.showError(this.requireView(), getString(R.string.feedback_send_fail))
+            findNavController().navigate(R.id.action_FragmentCreateThread_to_FragmentMessageThreads)
         }
     }
 
